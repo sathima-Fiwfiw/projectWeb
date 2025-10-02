34685ebdaf8c4735b759e5ae94d089ff20a7819f
Natthakit47 <66011212019@msu.ac.th>
2025-10-03 02:42:15 +0700

    เพิ่ม

---
+++ src/app/admin/editgame/editgame.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editgame',
+  imports: [],
+  templateUrl: './editgame.html',
+  styleUrl: './editgame.scss'
+})
+export class Editgame {
+
+}
